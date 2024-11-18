6899d613f069b8a9b60f2325826ec480019ea6c4
HieuNguyen06nd <nguyenhieu06.nd@gmail.com>
2024-11-18 17:31:31 +0700

    v1

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:26,
+    dots:false,
+    autoWidth:false,
+    callbacks:false,
+    nav:true,
+    autoWidth:false,
+    freeDrag:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
